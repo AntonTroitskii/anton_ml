04f3b9ce5484f352fb5883c7e57b751fb99d4ee7
Антон Троицкий <a.troitskiy@oncology.local>
2024-04-03 11:09:02 +0300

    main. update pytorch files module.

--- pytorch/files.py
+++ pytorch/files.py
@@ -2,6 +2,6 @@
 import pandas as pd
 
 
-def save_dict_as_cvs(data, file_path):
-    data_df = pd.DataFrame(data)
+def save_results_as_csv(results, file_path):
+    data_df = pd.DataFrame(results)
     data_df.to_csv(file_path, index=True, index_label='epoch')
